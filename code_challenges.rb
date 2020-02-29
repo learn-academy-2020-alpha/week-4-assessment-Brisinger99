5ccc10da6c91acb6ba11725e749080ef159a018f
learn <learn@LEARN-09.local>
2020-02-28 16:11:12 -0800

    Finished

--- code_challenges.rb
+++ code_challenges.rb
@@ -3,27 +3,75 @@
 # --------------------1) Create a method that takes in an array and returns an array with only odd numbers sorted from least to greatest.
 
 
-
-
-
 fullArr1 = [4, 9, 0, '7', 8, true, 'hey', 7, 199, -9, false, 'hola']
 # Expected output: [-9, 7, 9, 199]
 fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # Expected output: [-823, 7, 23]
 
 
+def sorter arr
+    finish = []
+    arr.each do |value|
+        if value.is_a?(Integer)
+            if value % 2 != 0
+                finish.push(value)
+            end
+        end
+    end
+    finish.sort! {|x,y| x <=> y}
+end
+
+print sorter fullArr1
+puts ""
+print sorter fullArr2
+puts ""
+
 # --------------------2) Create a class called Bike that is initialized with a model, wheels, and a frame size. The default number of wheels is 2. Create a get_info method that returns a sentence with all the data from the bike object.
 
 
+class Bike
+    def initialize(model, frame, wheels=2)
+        @model = model
+        @wheels = wheels
+        @frame = frame
+        @speed = 0
+    end
 
+    def get_info
+    "The #@model bike has #@wheels wheels and a #@frame frame."
+    end
 
+    def ring_bell
+        'Beep beep!'
+    end
+
+    def speed
+        @speed
+    end
+
+    def pedal_faster
+        @speed += 10
+    end
+
+    def brake
+        if @speed - 15 < 0
+            @speed = 0
+        else
+            @speed -= 15
+        end
+    end
+
+end
+
+my_bike = Bike.new('Trek', '168cm')
+puts my_bike.get_info
 
 # Expected output example: 'The Trek bike has 2 wheels and a 168cm frame.'
 
 
 # -------------------3) Add a bell to the bike class and create a method that will ring the bell when the method is called.
 
-
+puts my_bike.ring_bell
 
 
 
@@ -34,7 +82,7 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 
 
 
-
+puts my_bike.speed
 
 # Expected output example: my_bike.speed => 0
 
@@ -42,7 +90,10 @@ fullArr2 = ['hello', 7, 23, -823, false, 78, nil, '67', 6, 'Number']
 # -------------------5) Add the ability to pedal faster and brake. The pedal_faster method should increase the speed. The brake method should decrease the speed. The bike cannot go negative speeds.
 
 
-
+puts my_bike.pedal_faster
+puts my_bike.pedal_faster
+puts my_bike.brake
+puts my_bike.brake
 
 
 # Expected output example: my_bike.pedal_faster 10 => 10
--- interview-questions.md
+++ interview-questions.md
@@ -6,36 +6,48 @@ Answer the following questions. First, without external resources. Challenge you
 
   Your answer:
 
+  An instance variable is only referenced inside of a class
+
   Researched answer:
 
-
+  An instance variable is a variable that is accessible in any instance method in a particular instance of a class
 
 2. What is a block in Ruby?
 
   Your answer:
 
+  A block is either a function or hash
+
   Researched answer:
 
-
+  A Ruby block is a way of grouping statements, and may appear only in the source adjacent to a method call; the block is written starting on the same line as the method call's last parameter (or the closing parenthesis of the parameter list). The code in the block is not executed at the time it is encountered
 
 3. Ruby has an implicit return. What does that mean?
 
   Your answer:
 
+  You don't need a return for the end of a function
+
   Researched answer:
 
-
+  when return isn't explicitly called within a method then Ruby returns the value of the last executed instruction in the method.
 
 4. What is object-oriented programming? How is it different than functional programming?
 
   Your answer:
 
+  object-oriented programming is baised on using classes and hashes
+
   Researched answer:
 
-
+  Functional programming and object-oriented programming uses a different method for storing and manipulating the data. In functional programming, data cannot be stored in objects and it can only be transformed by creating functions. In object-oriented programming, data is stored in objects.
 
 5. What is the difference between a class and an object?
 
   Your answer:
 
+  An object only contains information while a class has both information and methods.
+
   Researched answer:
+
+  A class defines object properties including a valid range of values, and a default value. A class also describes object behavior. An object is a member or an "instance" of a class
